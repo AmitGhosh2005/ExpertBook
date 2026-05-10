33400106fe2ffd7197b3c4af21902ab2cb871962
Amit Ghosh <ghoshamit22826@gmail.com>
2026-05-10 12:47:33 +0530

    new commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: "https://expert-book-coral.vercel.app",
   },
 })
 
